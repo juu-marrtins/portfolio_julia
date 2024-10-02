4581dd0ccdca62ae4d651ce3a802bf2d771ef3c4
Julia Martins <juliagabrielly.m.araujo@gmail.com>
2024-10-01 21:21:43 -0300

    Feat: 01 Aula JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMessage(){
+    return console.log("Olá Mundo!")
+}
+
+function handleButton() {
+    return console.log("Você clicou no botão!")
+}
+
+getMessage();
+
+console.log("%cOlaMundo","color:yellow;background-color:black;padding:1hem;");
